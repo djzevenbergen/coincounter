0b2a5841f13c2fdc298753092a19bb6d48f14e59
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-06-22 14:54:49 -0700

    finish tests

--- _tests_/coincounter.test.js
+++ _tests_/coincounter.test.js
@@ -1,6 +1,9 @@
 import { coinCounter } from './../src/coincounter.js';
 import { coinCounter2 } from './../src/coincounter.js';
-import { Counter } from './../src/coincounter.js';
+import { quarter } from './../src/coincounter.js';
+import { dime } from './../src/coincounter.js';
+import { nickel } from './../src/coincounter.js';
+import { penny } from './../src/coincounter.js';
 
 test('verifies that the Age object is created with the correct age property', function () {
   let thisArray = [2, 1, 1, 1];
@@ -12,5 +15,13 @@ test('verifies that coinCounter2 returns an array conatining the number of quart
 });
 
 test('verifies that quarter works as expected', function () {
-  expect(Counter.quarter(.55)).toEqual(2);
+  expect(quarter(.55)).toEqual(2);
+});
+
+test('verifies that dime works as expected', function () {
+  expect(dime(.61)).toEqual(6);
+});
+
+test('verifies that nickel works as expected', function () {
+  expect(nickel(.17)).toEqual(3);
 });
--- src/coincounter.js
+++ src/coincounter.js
@@ -1,62 +1,59 @@
-export class Counter {
-
-
-  coinCounter = (money, moneyArray = [0, 0, 0, 0]) => {
-    money = Math.ceil(money.toFixed(2) * 100) / 100;
-    const val = [0, 0];
-    if (money == 0) {
-      console.log(moneyArray);
-      return moneyArray;
-    } else if (money >= .25) {
-      val[0] = .25;
-      val[1] = 0;
-    } else if (money >= .1) {
-      val[0] = .1;
-      val[1] = 1;
-    } else if (money >= .05) {
-      val[0] = .05;
-      val[1] = 2;
-    } else {
-      val[0] = .01;
-      val[1] = 3;
-    }
-    money -= val[0];
-    moneyArray[val[1]] += 1;
-    return coinCounter(money, moneyArray);
+export const coinCounter = (money, moneyArray = [0, 0, 0, 0]) => {
+  money = Math.ceil(money.toFixed(2) * 100) / 100;
+  const val = [0, 0];
+  if (money == 0) {
+    console.log(moneyArray);
+    return moneyArray;
+  } else if (money >= .25) {
+    val[0] = .25;
+    val[1] = 0;
+  } else if (money >= .1) {
+    val[0] = .1;
+    val[1] = 1;
+  } else if (money >= .05) {
+    val[0] = .05;
+    val[1] = 2;
+  } else {
+    val[0] = .01;
+    val[1] = 3;
   }
+  money -= val[0];
+  moneyArray[val[1]] += 1;
+  return coinCounter(money, moneyArray);
+}
 
 
-  counter = (denomination) => {
-    return (money) => {
-      return Math.floor((money / denomination))
-    }
+export const counter = (denomination) => {
+  return (money) => {
+    return Math.floor((money / denomination))
   }
+}
 
-  quarter = counter(.25);
-  dime = counter(.1);
-  nickel = counter(.05);
+export const quarter = counter(.25);
+export const dime = counter(.1);
+export const nickel = counter(.05);
 
-  coinCounter2 = (money) => {
-    const counts = [0, 0, 0, 0];
-    let mLeft = money;
-    let index = 0;
+export const coinCounter2 = (money) => {
+  const counts = [0, 0, 0, 0];
+  let mLeft = money;
+  let index = 0;
 
-    counts[index] = quarter(mLeft);
-    mLeft -= (counts[index] * .25).toFixed(2);
-    index++;
+  counts[index] = quarter(mLeft);
+  mLeft -= (counts[index] * .25).toFixed(2);
+  index++;
 
-    counts[index] = dime(mLeft);
-    mLeft -= (counts[index] * .10).toFixed(2);
-    index++;
+  counts[index] = dime(mLeft);
+  mLeft -= (counts[index] * .10).toFixed(2);
+  index++;
 
-    counts[index] = nickel(mLeft);
-    mLeft -= (counts[index] * .05).toFixed(2);
-    index++;
+  counts[index] = nickel(mLeft);
+  mLeft -= (counts[index] * .05).toFixed(2);
+  index++;
 
-    counts[index] = Math.ceil((mLeft * 100).toFixed(1));
+  counts[index] = Math.ceil((mLeft * 100).toFixed(1));
 
-    return counts;
-  }
+  return counts;
+}
 
   // console.log(coinCounter2(.76));
   // console.log(coinCounter2(1.42));
@@ -88,4 +85,3 @@ export class Counter {
 
 
   // coinCounter(.66);
-}
